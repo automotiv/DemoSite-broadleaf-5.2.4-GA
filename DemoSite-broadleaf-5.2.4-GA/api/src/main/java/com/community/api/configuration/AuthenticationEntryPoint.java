e34ad6eac97239f7f7b8da52691aeb043a5cac66
Prakash Ponali <p.ponali@gmail.com>
2018-07-30 19:56:59 +0530

    disabling security

---
+++ DemoSite-broadleaf-5.2.4-GA/api/src/main/java/com/community/api/configuration/AuthenticationEntryPoint.java
@@ -0,0 +1,38 @@
+package com.community.api.configuration;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
+
+	private static final Log LOG = LogFactory.getLog(AuthenticationEntryPoint.class);
+	
+	@Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
+      throws IOException, ServletException {
+		LOG.info("In commence method" + authEx);
+        response.addHeader("WWW-Authenticate", "Basic realm=" +getRealmName());
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        PrintWriter writer = response.getWriter();
+        writer.println("HTTP Status 401 - " + authEx.getMessage());
+    }
+
+	@Override
+    public void afterPropertiesSet() throws Exception {
+		LOG.info("In afterPropertiesSet method");
+        setRealmName("DeveloperStack");
+        super.afterPropertiesSet();
+    }
+
+}
